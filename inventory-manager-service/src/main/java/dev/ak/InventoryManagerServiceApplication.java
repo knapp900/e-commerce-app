2e84da5cca04b601e24eee9575ba3ea72d93a14e
Arthur Knapp <knapp900@gmail.com>
2024-08-21 10:15:11 +0300

    Added first module

---
+++ inventory-manager-service/src/main/java/dev/ak/InventoryManagerServiceApplication.java
@@ -0,0 +1,12 @@
+package dev.ak;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagerServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryManagerServiceApplication.class, args);
+    }
+}
